3cdd487dc5f1cd0f1ddec92819bface1436056ad
krishani indrachapa <Krishani.Indrachapa@syscolabs.com>
2021-11-04 22:27:25 +0530

    remove unwanted console.logs

--- src/repository/MovieRepository.js
+++ src/repository/MovieRepository.js
@@ -6,18 +6,15 @@ export const getAllMovies = async () => {
 };
 
 export const getMovie = async (id) => {
-  console.log('[MovieRepository] Retreiving movie by id', id);
   return await getRepository('Movie').find({ id });
 };
 
 export const insertMovie = async (movie) => {
-  console.log('[MovieRepository] Adding a movie');
   await getRepository('Movie').save(movie);
   return movie;
 };
 
 export const deleteMovie = async (id) => {
-  console.log('[MovieRepository] Deleting a movie');
   await getRepository('Movie').remove({ id });
   return null;
 };
